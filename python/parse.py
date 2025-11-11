051d908e7c9b9b03618c4a807a43269ac4b6c6cc
Tyler <tylerbrady2005@gmail.com>
2025-11-11 14:51:25 +0000

    heehee

--- python/parse.py
+++ python/parse.py
@@ -1,2 +1,19 @@
 import pandas as pd
 
+
+data = None
+
+def load(word):
+    global data
+
+
+    try:
+        with open(word, 'r', encoding='latin1') as f:
+            text = f.read()
+            print("Data loaded")
+        return text
+    except FileNotFoundError:
+        print("Data not loaded")
+        return None
+    
+load(/abc_books)
